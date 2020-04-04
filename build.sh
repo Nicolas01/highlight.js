5e6fbc7d9870fb7932e3a7ccfff90e836a6e2da4
Nicolas <2211631+Nicolas01@users.noreply.github.com>
2020-04-04 23:35:43 +0200

    add tsql

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-node tools/build.js -t browser :common accesslog autohotkey dns dos django excel irc less powershell tex typescript vbscript yaml
+node tools/build.js -t browser :common accesslog autohotkey dns dos django excel irc less powershell tex tsql typescript vbscript yaml

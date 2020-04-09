78d248882303e3549a2d81f523686be62ce9881c
Nicolas <2211631+Nicolas01@users.noreply.github.com>
2020-04-09 18:44:44 +0200

    build script and style for dev test

--- build.sh
+++ build.sh
@@ -1,1 +1,24 @@
-node tools/build.js -t browser :common accesslog autohotkey dns dos django excel irc less powershell tex tsql typescript vbscript yaml
+# common: Apache, Bash, CoffeeScript, C++, C#, CSS, Diff, Go, HTTP, INI, Java, JavaScript, JSON, Kotlin, Less, Lua, 
+#         Makefile, Markdown, Nginx, Objective-C, Perl, PHP, plaintext, Properties, Python, Ruby, Rust, SCSS, Shell Session, 
+#         SQL, Swift, TypeScript, XML, YAML
+
+#node tools/build.js -t browser :common accesslog autohotkey dns dos django excel irc less powershell razor tex tsql typescript vbscript yaml
+
+node tools/build.js -t browser \
+accesslog apache autohotkey \
+bash \
+cpp cs css \
+diff django dos dns \
+excel \
+ini irc \
+javascript json \
+less \
+markdown \
+nginx \
+php plaintext powershell properties \
+razor ruby \
+scss sql \
+tex tsql typescript \
+vbscript \
+xml \
+yaml
